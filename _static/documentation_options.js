37b7acf284f8d35b916cc28c68d39e0c948ab7bc
hellowac <hellowac@users.noreply.github.com>
2024-10-21 13:32:33 +0000

    deploy: 8a8d58c8ce71cf4aa7a622681da888eeb514a8b3

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.13.0+dev.8a8d58c8ce71cf4aa7a622681da888eeb514a8b3',
+    LANGUAGE: 'zh-CN',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
